36c23fc3aa549e0ad5de69ac9265b445b8735442
kay <kayshiro@live.no>
2022-01-14 07:05:25 +0100

    progressBar

--- app/src/main/java/com/kay/a7minutesworkout/fragments/ExerciseFragment.kt
+++ app/src/main/java/com/kay/a7minutesworkout/fragments/ExerciseFragment.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.kay.a7minutesworkout.databinding.FragmentExerciseBinding
-
 
 class ExerciseFragment : Fragment() {
     private var _binding: FragmentExerciseBinding? = null
@@ -19,7 +20,18 @@ class ExerciseFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentExerciseBinding.inflate(inflater, container, false)
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // toolbar setup
+        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.toolbarExercise)
+
+        // navigate back with navHost
+        binding.toolbarExercise.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/kay/a7minutesworkout/fragments/StartFragment.kt
+++ app/src/main/java/com/kay/a7minutesworkout/fragments/StartFragment.kt
@@ -1,11 +1,9 @@
 package com.kay.a7minutesworkout.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kay.a7minutesworkout.R
